7c075206bc70989ad150d8cf0f60d7a36fcb6036
Rodrigo Orrego <rorrego@gmail.com>
2013-12-17 14:09:41 -0300

    server

--- Gemfile
+++ Gemfile
@@ -1,4 +1,14 @@
-source :rubygems
+source 'http://rubygems.org'
 
+gem 'tilt', :git => 'git://github.com/rtomayko/tilt.git', :ref => '35129ad0b9895eef4e5b1a0b7053d4617f13aa71'
+gem "sinatra"
+gem 'sinatra-contrib'
+gem  'sinatra-partial'
+gem 'sinatra-static-assets'
+gem 'sinatra-env'
+gem 'will_paginate', '~> 3.0.5'
+gem 'will_paginate-bootstrap'
 gem "thin"
+gem "nokogiri"
+gem "httparty"
 gem "middleman", "~>3.0.0"
--- config.rb
+++ config.rb
@@ -42,7 +42,121 @@ configure :build do
 
 end
 
+require "sinatra"
+use Rack::CommonLogger
+require 'sinatra/contrib/all'
+require 'sinatra/partial'
+require 'sinatra/static_assets'
+require 'sinatra-env'
+require 'haml'
+require 'active_support/core_ext/string'
+require 'will_paginate'
+require 'will_paginate/array'
 
+
+module ApplicationHelper
+  def asset_server()
+    server = (Sinatra.env.development?)? 'http://localhost:4567' : 'http://sic-assets.metrik.cl'
+    return server
+
+  
+  end
+  def generate_url(type, value)
+    return "#" if type == 'domain' && @domains.length == 1
+    return "#" if type == 'rtype' && @types.length == 1
+    url = "/sinatra/search.html?query=#{@query}&page=#{@page}"
+    url << "&rtype=#{@rtype}" unless @rtype.blank?
+    url << "&domain=#{@domain}" unless @domain.blank?
+    url << "&#{type}=#{value}"
+    return url
+  end
+
+  def params_for_will_paginate()
+    params = {:query  => @query}
+    params.merge!({:domain => @domain}) unless @domain.blank?
+    params.merge!({:rtype => @rtype}) unless @rtype.blank?
+    return params
+  end
+
+end
+
+class MySinatra < Sinatra::Base
+  register Sinatra::Contrib
+  register Sinatra::Partial
+  register Sinatra::StaticAssets
+  include WillPaginate::Sinatra::Helpers
+  helpers ApplicationHelper
+
+require "nokogiri"
+  get "/" do
+
+  end
+  post "/search.html" do
+  
+
+    @page = params[:page] || 1
+    @per_page = 30
+    indx = @per_page*(@page-1)+1
+    options = {:indx  => indx, :bulk_size => @per_page, :institution => 'CONICYT'}
+    @query = params[:query]
+    @domain = params[:domain]
+    @rtype = params[:type]
+    options.merge!({:query => "any,contains,#{@query}"})
+    unless params[:domain].blank?
+      options.merge!({:query => "facet_domain,exact,#{@domain}"})
+    end
+    unless params[:type].blank?
+      options.merge!({:query => "facet_rtype,exact,#{@rtype}"})
+    end
+
+    #response = HTTParty.get('https://www.pcfactory.cl/', options)
+    # return 'result'
+
+    #CONSTRUIR LA url
+
+    #PARA OBTENER EL xml SIN ESPACIOS NI SALTOS DE LINEA
+    if Sinatra.env.development?
+      aux = File.read("primo.xml").gsub(/>\s+</,'><')
+    else
+      #Production
+      aux = HTTParty.get('http://primo.gsl.com.mx:1701/PrimoWebServices/xservice/search/brief', options).gsub(/>\s+</,'><')
+    doc = Nokogiri::XML(aux)
+    
+    basic = doc.xpath(".//xmlns:DOCSET")
+    @total_results = basic.xpath(".//@TOTALHITS").first.value
+    @total= @total_results.reverse.gsub(/(\d{3})(?=\d)/, '\\1.').reverse
+
+
+
+    domain = doc.xpath(".//xmlns:FACET[@NAME='domain']")
+    numero_de_dominios = domain.xpath('.//@COUNT').first.value
+
+    domains   = domain.xpath(".//xmlns:FACET_VALUES")
+    @domains  = domains.collect{|d| {:key => d.xpath('.//@KEY').first.value, :value => d.xpath('.//@VALUE').first.value}}.sort_by{|x| x[:key]}
+    type     = doc.xpath(".//xmlns:FACET[@NAME='rtype']")
+    types     = type.xpath(".//xmlns:FACET_VALUES")
+    @types    = types.collect{|d| {:key => d.xpath('.//@KEY').first.value, :value => d.xpath('.//@VALUE').first.value}}.sort_by{|x| x[:key]}
+    doc.remove_namespaces!
+    record = doc.xpath(".//record")
+    @records = record.collect{|r| {:title => r.xpath(".//display/title").first.text,
+                                    :authors => r.xpath(".//display/creator").first.text.gsub(',',', '),
+                                    :publisher => r.xpath(".//display/publisher").first.text,
+                                    :creation_date => r.xpath(".//display/creationdate").first.text,
+                                    :volume => r.xpath(".//display/version").first.text
+                                  }
+                              }
+    @page_results = WillPaginate::Collection.create(@page, @per_page, @total_results) do |pager|
+      pager.replace(@records)
+    end
+
+
+    haml :search
+  end
+end
+
+map "/sinatra" do  
+  run MySinatra
+end 
 
 
 ###
